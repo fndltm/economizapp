963fc4ed60cb9d517a2b86f5d1e00bcdfb0a6830
Luiz Rodrigues <luiz.gmr.dev@gmail.com>
2022-05-08 17:35:30 -0300

    Correções server

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
-app.use(express.static(`${__dirname}/wwww`));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/wwww/index.html`));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
-app.listen(process.env.PORT || 4200);
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
