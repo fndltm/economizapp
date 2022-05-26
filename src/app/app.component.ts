f8feb063d9e2c472cc4f94396492e792729d33cd
Luiz Rodrigues <luiz.gmr.dev@gmail.com>
2022-05-25 21:24:34 -0300

    Remoção splash screen aplicativos híbridos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { UtilsService } from '@utils/utils.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
